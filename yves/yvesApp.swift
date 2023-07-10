375f432bfd93e1d40fe6631997c4a0e972f9a2a2
Anthony Laflamme <anthonylaflamme@civalgo.com>
2023-07-10 19:56:17 -0400

    Initial Commit

---
+++ yves/yvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  yvesApp.swift
+//  yves
+//
+//  Created by Anthony Laflamme on 2023-07-10.
+//
+
+import SwiftUI
+
+@main
+struct yvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
